76a5116e15cbc8d0ce5ef1ebddd6af59f5fe8b2f
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-07 11:41:48 +0100

    chore:Funcoes com objectos e argumentos

--- src/topic/03-functions.ts
+++ src/topic/03-functions.ts
@@ -16,13 +16,34 @@ function multiply( firstNumber: number, secondNumber?: number, base: number = 2 
    return firstNumber * base;
 }
 
-const result: number = addNumbers(1,2);
-const result2: string = addNumbersArrow(2,3);
-const multiplyResult: number = multiply(5); 
+// const result: number = addNumbers(1,2);
+// const result2: string = addNumbersArrow(2,3);
+// const multiplyResult: number = multiply(5); 
+//console.log({ result, result2, multiplyResult })
 
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
 
-console.log({ result, result2, multiplyResult })
+const healCharacter = ( character: Character, amount: number ) => {
 
+   character.hp += amount;
+
+}
+
+const strider: Character = {
+    name: 'Strider', 
+    hp: 50, 
+    showHp() {
+      console.log(`Pontos de vida ${ this.hp }`);
+    }
+}
+
+healCharacter( strider, 20)
+
+strider.showHp();
 
 
 export {}
